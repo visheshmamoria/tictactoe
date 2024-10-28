a0b559436e362c93437666ec92d2e928f91cf0a4
visheshmamoria <visheshmamoria@gmail.com>
2024-10-28 12:44:26 +0530

    New changes

--- tut.py
+++ tut.py
@@ -1,35 +1,12 @@
-x = 1
-def test():
-    x = 2
-test()
-print(x)
+game  = [[1, 0, 1],
+         [0, 2, 0],
+         [2, 2, 0]]
 
+def win(current_game):
+    for row in current_game:
+        print(row)
+        
+        if row.count(row[0]) == len(row) and row[0] != 0:
+            print("Congrats jackass")
 
-x = 1
-def test():
-    global x
-    x = 2
-test()
-print(x)
-
-
-x = [1]
-def test():
-    x = [2]
-test()
-print(x)
-
-
-x = [1]
-def test():
-    global x
-    x = [2]
-test()
-print(x)
-
-
-x = [1]
-def test():
-    x[0] = 2
-test()
-print(x)
+win(game)
